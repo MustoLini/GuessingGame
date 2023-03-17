69d5181457b6128d8b0b3d0654cf08835026cee4
Johannes <danielssonjohannes@gmail.com>
2023-03-17 19:22:06 +0100

    Format and add @Column

--- src/main/java/com/example/guessinggame/Person.java
+++ src/main/java/com/example/guessinggame/Person.java
@@ -6,23 +6,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Table(name = "personner")
 public class Person {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @Column(name = "name")
     private String name;
-
     @OneToMany
     private List<Result> result;
-
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public Long getId() {
         return id;
     }
 
+    public String getName() {
+        return name;
+    }
 }
