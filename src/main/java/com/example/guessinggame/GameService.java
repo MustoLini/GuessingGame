fbf688be125f243537d80347588a69fa775b19c7
Johannes Danielsson <danielssonjohannes@gmail.com>
2023-03-17 12:07:04 +0100

    Updating the GameService.java

--- src/main/java/com/example/guessinggame/GameService.java
+++ src/main/java/com/example/guessinggame/GameService.java
@@ -4,11 +4,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.annotation.SessionScope;
 
+import java.util.Random;
+
 @Service
 @SessionScope
 public class GameService {
     @Autowired
     PersonRepository personRepository;
+    Random random = new Random();
+    private Person person;
+    private int secret;
+
+    public GameService() {
+        init();
+    }
+
+    private void init() {
+        secret = random.nextInt(1, 100);
+    }
 
 
 }
