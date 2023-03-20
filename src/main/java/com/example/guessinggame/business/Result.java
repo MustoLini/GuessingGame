991fc1312b1c03b6190cb8323ab68af788b1874c
Johannes <danielssonjohannes@gmail.com>
2023-03-20 13:06:34 +0100

    Adding  @GeneratedValue(strategy = GenerationType.IDENTITY) into result might not need it

--- src/main/java/com/example/guessinggame/business/Result.java
+++ src/main/java/com/example/guessinggame/business/Result.java
@@ -1,14 +1,15 @@
-package com.example.guessinggame;
+package com.example.guessinggame.business;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Result {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private int result;
