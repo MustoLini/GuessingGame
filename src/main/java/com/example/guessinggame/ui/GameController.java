799bec0b7d896b8810ad66a5801d7578cde859b4
Johannes <danielssonjohannes@gmail.com>
2023-03-20 14:36:33 +0100

    Now Adding to database work but not yet guessLowOrHigh

--- src/main/java/com/example/guessinggame/ui/GameController.java
+++ src/main/java/com/example/guessinggame/ui/GameController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -18,15 +19,28 @@ public class GameController {
     // Need to have Get mapping here also that will get
 
     @PostMapping("/login")
-    public String login(@ModelAttribute Person person, Model model) {
+    public String login(@RequestParam String name, Model model) {
         // Login in method;
-        gameService.add(person);
+        gameService.add(name);
         model.addAttribute("person", new Person());
-        return "websiteForName";
+        return "guessWebsite";
     }
+    @PostMapping("/guess")
+    public String guessLowOrHigh(@RequestParam int guess, Model model){
+        String guessText =gameService.guessHighOrLow(guess);
+        model.addAttribute("guess", guessText);
+        return "guessWebsite";
+    }
+
+
     @GetMapping("/login")
     public String addLogin(Model model){
         model.addAttribute("person", new Person());
-        return "websiteForName";
+        return "guessWebsite";
+    }
+    @PostMapping ("/all")
+    String getAll(Model model){
+        model.addAttribute("gueesList", gameService.getAll());
+        return "showAverageAndTries";
     }
 }
