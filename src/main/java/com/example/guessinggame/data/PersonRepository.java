db5dcf94870187d4a5fb5bcb84fd534e1b0cf217
Johannes Danielsson <danielssonjohannes@gmail.com>
2023-03-21 22:37:13 +0100

    Changing DataStuck to Optional Instead of List

--- src/main/java/com/example/guessinggame/data/PersonRepository.java
+++ src/main/java/com/example/guessinggame/data/PersonRepository.java
@@ -4,8 +4,10 @@ import com.example.guessinggame.business.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
+    Optional<Person> findByName(String name);
    // Find By name Add att later Point
-    List<Person> findByName(String name);
+
 }
